d1455a7612fa04b1d54e3ba1754b4ca3bdf8c390
garchomp-game <otoka.japanesedrum@gmail.com>
2023-11-20 15:17:25 +0900

    commit

---
+++ resources/js/Components/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="flex flex-col">
+            <div class="mb-4">
+                <TextInput
+                    ref="input"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    :value="modelValue"
+                    @input="$emit('update:modelValue', $event.target.value)"
+                    placeholder="新しいタスクを追加"
+                />
+                </div>
+            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="task1">
+                        タスク1
+                    </label>
+                    <p class="text-gray-700 text-base">タスク1の詳細</p>
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="task2">
+                        タスク2
+                    </label>
+                    <p class="text-gray-700 text-base">タスク2の詳細</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script setup>
+import TextInput from "@/Components/TextInput.vue"
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,20 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import ToDoList from '@/Components/ToDoList.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="ToDoリストメインページ">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                ToDoリスト
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <ToDoList />
                 </div>
             </div>
         </div>
